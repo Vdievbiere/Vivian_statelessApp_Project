a0f9cdbcb73edeca005d0d5900e524a4d8ef3f0e
Vivian Dievbiere <vdievbiere@yahoo.com>
2022-10-28 00:27:22 -0400

    adding updated code

--- main.tf
+++ main.tf
@@ -18,12 +18,21 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+# data "aws_subnet_ids" "private" {
+#   vpc_id = module.vpc.vpc_id
+
+#   tags = {
+#     Tier = "Private"
+#   }
+# }
+
+
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "my-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = var.vpc_cidr
 
   azs             = data.aws_availability_zones.available.names
   private_subnets = var.private_cidr
@@ -48,3 +57,36 @@ module "web_server_sg" {
 
   ingress_cidr_blocks = ["10.10.0.0/16"]
 }
+
+#creating Ec2 Instance
+
+resource "aws_instance" "web" {
+  ami           = var.ami_ids
+  #availability_zone = slice(data.aws_availability_zones.available.names, 0,3)
+  availability_zone = var.azs[count.index]
+  instance_type = var.instance_type
+  count = length(var.ec2_name_tag)     ##https://stackoverflow.com/questions/71359239/terraform-how-to-output-multiple-value
+  # subnet_id =data.aws_subnet_ids.private
+  subnet_id = module.vpc.private_subnets[count.index]                                                                       
+
+  tags = {
+    Name = var.ec2_name_tag[count.index]
+    
+  }
+}
+
+#EC2 LB target group
+resource "aws_lb_target_group" "web_target_group" {
+  name     = "web-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+# ### TARGET GROUP ATTACHMENT
+# resource "aws_lb_target_group_attachment" "web_tg_attach" {
+#   target_group_arn = aws_lb_target_group.web_target_group.arn
+#   target_id        = aws_instance.web[count.index]
+#   port             = 80
+# }
+
--- variables.tf
+++ variables.tf
@@ -11,6 +11,41 @@ variable "public_cidr" {
        description = "public cidr variables"
        default= ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
     }
-    
-    
+    # Below is the variables blocks for Ec2
+variable "ec2_name_tag" {
+  default = ["web1", "web2","web3"]
+}
+
+variable "instance_type" {
+            #   0          1          2
+  default = "t2.micro"
+}
+
+variable "ami_ids" {
+  default =  "ami-09d3b3274b6c5d4aa"
    
+  }
+
+variable "azs" { 
+    default = {
+        0 = "us-east-1a"
+        1 = "us-east-1b"
+        2 = "us-east-1c"
+    }
+}
+
+
+    
+   ##  Variable for VPC
+   variable "vpc_cidr"{
+      type = string
+      default = "10.0.0.0/16"
+   }
+
+   # ##VPC ID 
+   # # variable "vpc_id"{
+   #    type = string
+   #    default = module.vpc.vpc_id
+
+   # }
+ 
